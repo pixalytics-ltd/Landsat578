d8250577213819921e579638e79fc4ccea9e29be
slavender <slavender@pixalytics.com>
2019-08-03 23:17:36 +0100

    Update update_landsat_metadata.py

--- landsat/update_landsat_metadata.py
+++ landsat/update_landsat_metadata.py
@@ -94,7 +94,7 @@ def split_list(_list=LATEST):
         try:
             chunk = df.get_chunk(chunksize)
             fc = chunk[chunk.COLLECTION_NUMBER != 'PRE']
-            if '*LANDSAT*' in fc:
+            if 'LANDSAT' in fc.SPACECRAFT_ID.values:
                 sats = unique(fc.SPACECRAFT_ID).tolist()
             else:
                 sats = []
